8d23a100746578029207c06be7c250cea5dddbda
yladev <yanlinnaung881@gmail.com>
2023-01-08 14:59:52 +0630

    code-edit-line spacing

--- src/day2/Operators/Logical_Op.java
+++ src/day2/Operators/Logical_Op.java
@@ -2,8 +2,8 @@ package day2.Operators;
 
 public class Logical_Op {
     public static void main(String[] args) {
-        boolean bool1=true,
-        bool2=false;
+        boolean bool1 = true,
+        bool2 = false;
 
         //And &&//
         System.out.println(bool1 && bool2);
