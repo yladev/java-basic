235f18616f9dbe183586d83e77a312c75898cf69
yladev <yanlinnaung881@gmail.com>
2023-01-14 13:54:52 +0630

    update-relational operator

--- src/day3/operators/Arithmetic_Operator.java
+++ src/day3/operators/Arithmetic_Operator.java
@@ -1,6 +1,6 @@
 package day3.operators;
 
-public class Arithmetic {
+public class Arithmetic_Operator {
     public static void main(String[] args) {
         int num1 = 5;
         int num2 = 3;
---
+++ src/day3/operators/Relational_Operator.java
@@ -0,0 +1,16 @@
+package day3.operators;
+
+public class Relational_Operator {
+    public static void main(String[] args) {
+        //  < > <= >= == != InstanceOf
+
+        int num1 = 15;
+        int num2 = 14;
+        System.out.println(num1 > num2);
+        System.out.println(num1 < num2);
+        System.out.println(num1 >= num2);
+        System.out.println(num1 <= num2);
+        System.out.println(num1 == num2);
+        System.out.println(num1 != num2);
+    }
+}
