0d1c8b2c027a3e40cb1a6e3b98b9e8ff65701940
yladev <yanlinnaung881@gmail.com>
2023-01-14 14:16:21 +0630

    update-assignment operator

---
+++ src/day3/operators/Assignment_Operator.java
@@ -0,0 +1,30 @@
+package day3.operators;
+
+public class Assignment_Operator {
+    public static void main(String[] args) {
+        //  = == += -= *= /= %=
+        int a = 10;
+        a += 10;
+        System.out.println(a);
+
+        int b = 20;
+        b -= 10;
+        System.out.println(b);
+
+        int c = 30;
+        c = 50;
+        System.out.println(c);
+        
+        int e = 10;
+        e *= 40;
+        System.out.println(e);
+
+        int f = 10;
+        f /= 5;
+        System.out.println(f);
+
+        int g = 100;
+        g %= 9;
+        System.out.println(g);
+    }
+}
