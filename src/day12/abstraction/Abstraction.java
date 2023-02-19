9ac0f877f3baa6a2366d69ef48b15eb26ac4df63
yladev <yanlinnaung881@gmail.com>
2023-02-19 14:29:01 +0630

    update-abstract class

---
+++ src/day12/abstraction/Abstraction.java
@@ -0,0 +1,40 @@
+package day12.abstraction;
+
+abstract  class A{
+    abstract void doIt();
+    void making(){
+        System.out.println("This is making");
+    };
+}
+
+public class Abstraction extends A {
+    @Override
+    void doIt() {
+        System.out.println("This is doing HomeWork");
+    }
+    public static void main(String[] args) {
+        Abstraction abstraction = new Abstraction();
+        abstraction.doIt();
+        abstraction.making();
+    }
+}
+
+class C extends A {
+    @Override
+    void doIt() {
+        System.out.println("This is doIT from A");
+    }
+    @Override
+    void making() {
+        System.out.println("This is making from A");
+    }
+    public static void main(String[] args) {
+        C c = new C();
+        c.doIt();
+        c.making();
+
+        Abstraction abstraction = new Abstraction();
+        abstraction.doIt();
+        abstraction.making();
+    }
+}
