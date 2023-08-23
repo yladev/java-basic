39550d8b100020e7f17c65f8cf869e8bb9782da9
yladev <yanlinnaung881@gmail.com>
2023-08-23 16:39:48 +0630

    code refactor

---
+++ src/day6/Multiplication.java
@@ -0,0 +1,12 @@
+package day6;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int multiplier = 2;
+
+        for (int i = 1; i <= 10; i++) {
+            int result = multiplier * i;
+            System.out.println(multiplier + " * " + i + " = " + result);
+        }
+    }
+}
